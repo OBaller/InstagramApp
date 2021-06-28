f66ac0f320a72e949ab8510c858c6dc11946f8a7
Naseem Oyebola <naseemoyebola@gmail.com>
2021-06-28 17:25:37 +0100

    feat: login page

--- InstagramApp/InstagramApp/Controllers/Onboarding/RegisterationViewController.swift
+++ InstagramApp/InstagramApp/Controllers/Onboarding/RegisterationViewController.swift
@@ -12,7 +12,7 @@ class RegisterationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
     
 }
